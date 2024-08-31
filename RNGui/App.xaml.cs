c21b04750a7ba096047eee190218f969b14374d7
Ria Cherry <lilyflowerangel@gmail.com>
2024-08-31 15:44:47 +0530

    About window pre-finalized.

--- RNGui/App.xaml.cs
+++ RNGui/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System;
+using System.Windows;
 
 namespace RNGui;
 
@@ -7,4 +9,26 @@ namespace RNGui;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        // Application version
+
+        Version version = Assembly.GetExecutingAssembly().GetName().Version;
+        Resources["AssemblyVersion"] = version.ToString();
+
+        // Platform
+
+        string platform = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
+        Resources["FrameworkVersion"] = platform;
+        /*
+#if NET5_0_OR_GREATER
+        string platform = Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
+#elif NETFRAMEWORK
+        string platform = $".NET Framework {Environment.Version}";
+#endif
+        Resources["FrameworkVersion"] = platform;
+        */
+    }
 }
